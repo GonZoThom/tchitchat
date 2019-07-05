f212188cb19576dc21686f25b07605f3775e9e4e
GonZoThom <gonz.thom@gmail.com>
2019-07-05 10:29:50 +0200

    Fix router Home

--- imports/ui/modules/Home.jsx
+++ imports/ui/modules/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
+// import { Meteor } from 'meteor/meteor';
+// import { withTracker } from 'meteor/react-meteor-data';
 
 const Home = () => {    
-    console.log(userId);
+    // console.log(userId);
     return (
         <div>
             <h2>THIS IS A LANDING PAGE !</h2>
@@ -11,6 +11,7 @@ const Home = () => {
     )
 };
 
-export default withTracker(() => ({
-    userId: Meteor.userId(),
-}))(Home);
+export default Home;
+// export default withTracker(() => ({
+//     userId: Meteor.userId(),
+// }))(Home);
