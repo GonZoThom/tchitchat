0166f254e9c1ac035f9895dbbd2c68cc836a429c
GonZoThom <gonz.thom@gmail.com>
2019-07-05 16:43:06 +0200

    Fix server > main.js and add content on Home page

--- imports/ui/modules/Home.jsx
+++ imports/ui/modules/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { Link } from 'react-router-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
 // console.log(userId);
@@ -7,6 +8,10 @@ const Home = () => {
     return (
         <div>
             <h2>THIS IS A LANDING HOME PAGE !</h2>
+            <p>Bienvenue sur Tchitchat - <br/> 
+                <em>L'application de messageire du futur</em></p>
+            <Link to="/signin" >Se Connecter</Link>
+            <Link to="/signup" >S'inscrire</Link>
         </div>
     )
 };
