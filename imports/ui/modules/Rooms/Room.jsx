3a21480c98e5dead4d98bded65b22bd44a87b5d0
GonZoThom <gonz.thom@gmail.com>
2019-07-24 16:59:30 +0200

    1: Add link to Chatbox in 'Room.jsx' / 2: Added css for the 'Room.jsx' display

--- imports/ui/modules/Rooms/Room.jsx
+++ imports/ui/modules/Rooms/Room.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ManageRoomsForm from './ManageRoomsForm';
+import { Link } from 'react-router-dom';
 
 const Room = ({ room, userId, show }) => (
     <div>
         {(show) ? (
             <article className="roomLink">
-                <h4>{room.name}</h4>
+                <p><h4>{room.name}</h4>
+                    <Link to={`/rooms/${room._id}`}>
+                        <button>go to the chat</button>
+                    </Link>
+                </p>
                 {(room.ownerId === userId) ?
                 <div>
                     <ManageRoomsForm roomId={room._id} modify='true' remove='true'/>
