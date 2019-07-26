e8ca08a6c5c65892ecb40cf94d4bd461584ceac0
GonZoThom <gonz.thom@gmail.com>
2019-07-26 15:44:36 +0200

    Adding a redirection after email verification on 'Verify.jsx'

--- imports/ui/modules/Account/Verify.jsx
+++ imports/ui/modules/Account/Verify.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 // import { withTracker } from 'meteor/react-meteor-data';
 
-const Verify = (props) => {
-    const token = props.match.params.token;
+const Verify = ({ match, history }) => {
+    const token = match.params.token;
 
     const checkMail = () => {
         Accounts.verifyEmail(token, function (err) {
             if (err != null) {
                 console.log(err.message);
             } else {
-                console.log('Thank you! Your email address has been confirmed.')
+                console.log('Thank you! Your email address has been confirmed.');
+                history.push('/rooms');
             }
         });
     };
