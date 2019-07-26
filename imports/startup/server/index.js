72f5a38ff1b401cc995447b0f3ecb7212827cdf8
GonZoThom <gonz.thom@gmail.com>
2019-07-26 15:25:45 +0200

    Inititalized the email verification on /server/index.js + check the verification on 'Verify.jsx'

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 import '/imports/api/messages/server/';
 import '/imports/api/rooms/server/';
+
+Accounts.config({
+    sendVerificationEmail: true
+});
+
+Accounts.emailTemplates.verifyEmail = {
+    subject() {
+        return "Activate your account now!";
+    },
+    text(user, url) {
+        console.log(url);
+        url = url.replace('/#/verify-email', '/verify');
+        console.log(url);
+        return `Hey ${user.username}! Verify your e-mail by following this link: ${url}`;
+    }
+};
 
 Meteor.startup(() => {});
--- imports/ui/modules/Account/Verify.jsx
+++ imports/ui/modules/Account/Verify.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-import { withTracker } from 'meteor/react-meteor-data';
+// import { withTracker } from 'meteor/react-meteor-data';
 
-const Verify = () => {
-    const user = Meteor.user();
+const Verify = (props) => {
+    const token = props.match.params.token;
 
-    // Checking - Debug
-    // console.log(Meteor.userId());    
-    // console.log(Meteor.user());
-    // console.log(`user mail : ${userEmail}`);  
-
-    // const verifyEmail = () => {
-    //     Accounts.sendVerificationEmail();
-    // };
+    const checkMail = () => {
+        Accounts.verifyEmail(token, function (err) {
+            if (err != null) {
+                console.log(err.message);
+            } else {
+                console.log('Thank you! Your email address has been confirmed.')
+            }
+        });
+    };
 
     return (
         <div>
             <p>Vérifier votre E-mail avant de pouvoir créer un salon => 
-                {/* <button onClick={verifyEmail}>VERIFIER</button> */}
+                <button onClick={checkMail}>VERIFIER</button>
             </p>
         </div>
     )
 };
 
-export default withTracker(() => ({
-    userId: Meteor.userId(),
-    user: Meteor.user(),
-}))(Verify);
+export default Verify;
