40424a4883e043ae9f765c69f420593c82e322c2
GonZoThom <gonz.thom@gmail.com>
2019-07-29 15:55:22 +0200

    Added a button on 'PrivateChat' in order to go back to the list

--- imports/ui/modules/Users/PrivateChat.jsx
+++ imports/ui/modules/Users/PrivateChat.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PrivateChat = () => {
     return (
         <div>
             <h2>PrivateChat PAGE !</h2>
+        <p>
+            <Link to={`/users`}>
+                <button>Retour liste</button>
+            </Link>
+        </p>
         </div>
     )
 };
