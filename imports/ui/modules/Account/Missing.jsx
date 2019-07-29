cae097b4e8b8048ae3ae02217595714063036462
GonZoThom <gonz.thom@gmail.com>
2019-07-29 16:48:59 +0200

    Tried some stuff...

--- imports/ui/modules/Account/Missing.jsx
+++ imports/ui/modules/Account/Missing.jsx
@@ -1,3 +1,49 @@
 import React from 'react';
+import { Accounts } from 'meteor/accounts-base';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 
-export default () => <div />
+const Missing = ({ match, user, email, userMail, mail, history }) => {
+    // const token = match.params.token;
+    
+    console.log('userMail : ' + userMail);
+    console.log('user : ' + user);
+    console.log('email : ' + email);
+    // console.log(email.map(mail =>{
+    //     mail.address;
+    // }));
+
+    const forgotMyPassword = () => {
+        Accounts.forgotPassword(email, function (err) {
+            if (err != null) {
+                console.log(err.message);
+            } else {
+                console.log('Thank you! Your password has been sent by mail.');
+            }
+        });
+    };
+
+    return (
+        <div>
+            <p>Retrouver mon mot de passe oublié :
+                {/* <button onClick={forgotMyPassword}>ENVOYER</button> */}
+            </p>
+            {/* {emails.map(email =>{
+                <span>{email.address}</span>;
+            })} */}
+        </div>
+    )
+};
+
+export default withTracker(() => {
+    const user = Meteor.user() || {};
+    const userMail = Meteor.user() || { emails: [{}] };
+    // const mail = Meteor.user().emails[0].address;
+    const email = Meteor.users.findOne({
+         _id: Meteor.userId()}, { fields: {emails: 1} });
+    return {
+        userId: Meteor.userId(),
+        user, email, userMail
+        // email: user[0].emails.address,
+    }
+})(Missing);
--- imports/ui/modules/Users/UsersList.jsx
+++ imports/ui/modules/Users/UsersList.jsx
@@ -3,15 +3,21 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import Loader from '/imports/ui/components/Loader';
 
-const UsersList = ({ users, userId }) => {
-    console.log(users.username);
-    
+// Template for one User:
+import User from "./User";
+
+const UsersList = ({ users, loading, userId }) => {
+    // console.log("loading : "+loading);
+        
     return (
         <div>
             <section>
                 <button onClick={Meteor.logout}>Logout</button>
                 <h3>USERS LIST</h3>
-                <Loader
+                {users.map(user =>
+                    <span>{user.username}<br /></span>
+                )}
+                {/* <Loader
                     loading={loading}
                     render={users.map(user =>
                         <User
@@ -19,18 +25,18 @@ const UsersList = ({ users, userId }) => {
                             name={user.username}
                         />
                     )}
-                />
+                /> */}
             </section>
         </div>
     )
 };
 
 export default withTracker(() => {
-    const usersPublication = Meteor.subscribe('allUsers');
-    const loading = !usersPublication.ready();
+    // const usersPublication = Meteor.subscribe('allUsers');
+    // const loading = !usersPublication.ready();
     return {
         userId: Meteor.userId(),
         users: Meteor.users.find({ }).fetch(),
-        loading,
+        // loading,
     }
 })(UsersList);
